aa457c28b36bfefd145fe3c0e712806228f850bc
nicolas <nicolasfr.privat@gmail.com>
2024-09-02 13:54:27 +0200

    feat: added default logging

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { default as Holidays } from 'date-holidays';
+import logger from './logger.js';
+
+logger.info('Starting Holiday Converter...');
 
 const hd = new Holidays();
-
-console.log(hd.getCountries())
