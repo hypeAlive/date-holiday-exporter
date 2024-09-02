f8ec9b252c5c03ab07c087a417ba2cf2bea21e92
nicolas <nicolasfr.privat@gmail.com>
2024-09-02 14:29:23 +0200

    feat: added json file export

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import {default as Holidays, HolidaysTypes} from 'date-holidays';
 import logger from './logger.js';
+import fs from 'fs';
+import path from "node:path";
+
+const YEAR = 2024;
+const OUTPUT_PATH = 'output';
 
 logger.info('Starting Holiday Converter...');
 
@@ -10,11 +15,22 @@ const options: HolidaysTypes.Options = {
 const hd = new Holidays('DE', options);
 
 try {
-    const holidays = hd.getHolidays(2024);
+
+    if (!fs.existsSync(OUTPUT_PATH)) {
+        fs.mkdirSync(OUTPUT_PATH, { recursive: true });
+        logger.info('Output directory created: ' + OUTPUT_PATH);
+    }
+
+    const holidays = hd.getHolidays(YEAR);
     if (holidays.length === 0) {
         logger.warn('No holidays found.');
     } else {
-        console.log('Holidays:', holidays);
+        const fileName = `holidays-${YEAR}.json`;
+        const filePath = path.join(OUTPUT_PATH, fileName);
+
+        fs.writeFileSync(filePath, JSON.stringify(holidays, null, 2));
+
+        logger.info('Holidays written to file ' + fileName);
     }
 } catch (error) {
     logger.error('Error fetching holidays:', error);
