65d4c0aafd2be4322b9d771d4639eed616832ac4
nicolas <nicolasfr.privat@gmail.com>
2024-09-02 14:01:20 +0200

    feat: added Holiday print to console Germany 2024

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,15 @@ import logger from './logger.js';
 
 logger.info('Starting Holiday Converter...');
 
-const hd = new Holidays();
+const hd = new Holidays('DE');
+
+try {
+    const holidays = hd.getHolidays(2024);
+    if (holidays.length === 0) {
+        logger.warn('No holidays found.');
+    } else {
+        console.log('Holidays:', holidays);
+    }
+} catch (error) {
+    logger.error('Error fetching holidays:', error);
+}
