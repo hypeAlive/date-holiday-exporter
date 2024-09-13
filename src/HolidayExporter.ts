6135b97f36720cf207c427ccf2ea8c44b53223c2
nicolas <nicolasfr.privat@gmail.com>
2024-09-13 10:32:59 +0200

    feat: transformed code into Builder and Exporter to customize Output

--- src/HolidayExporter.ts
+++ src/HolidayExporter.ts
@@ -1,4 +1,7 @@
-import {HolidaysTypes} from "date-holidays";
+import {default as Holidays, HolidaysTypes} from "date-holidays";
+import fs from "fs/promises";
+import logger from "./logger.js";
+import path from "node:path";
 
 interface Builder<A> {
     build(): A;
@@ -9,9 +12,14 @@ interface ExportBuilder<A> extends Builder<A> {
     setFileName(name: string): this;
 }
 
+type ExportHoliday = HolidaysTypes.Holiday & {
+    countryCode: string,
+    countryName: string
+};
+
 export class HolidayExportBuilder implements ExportBuilder<HolidayExporter> {
 
-    private exporter: HolidayExporter
+    private readonly exporter: HolidayExporter
 
     constructor() {
         this.exporter = new HolidayExporter();
@@ -22,17 +30,106 @@ export class HolidayExportBuilder implements ExportBuilder<HolidayExporter> {
     }
 
     setFileName(name: string): this {
+        this.anyExporter.fileName = name;
         return this;
     }
 
     setOutputPath(path: string): this {
+        this.anyExporter.outputPath = path;
         return this;
+    }
+
+    setYears(...years: number[]): this {
+        this.anyExporter.years = years;
+        return this;
+    }
+
+    setTypes(...types: HolidaysTypes.HolidayType[]): this {
+        this.anyExporter.types = types;
+        return this;
+    }
+
+    setCountries(...countries: string[]): this {
+        this.anyExporter.countries = countries;
+        return this;
+    }
+
+    modifyJson(modifier: (json: ExportHoliday) => object): this {
+        this.anyExporter.jsonModifier = modifier;
+        return this;
+    }
+
+    private get anyExporter(): any {
+        return this.exporter as any;
     }
 
 }
 
-class HolidayExporter {
-    export() {
+export class HolidayExporter {
 
+    private years: number[] = [new Date().getFullYear()];
+    private types: HolidaysTypes.HolidayType[] = ['public'];
+    private countries: string[] = Object.keys(new Holidays().getCountries());
+    private outputPath: string = 'output';
+    private jsonModifier: (json: ExportHoliday) => object = json => json;
+    private fileName: string | null = null;
+
+    static create(): HolidayExportBuilder {
+        return new HolidayExportBuilder();
+    }
+
+
+    async export() {
+        await this.ensureOutputDirectory();
+
+        const result: any[] = [];
+
+        this.countries.forEach(country => {
+           const holidays = this.fetchHolidaysForCountry(country);
+
+           result.push(...holidays.map(this.jsonModifier));
+        });
+
+        await this.writeHolidaysToFile(result);
+
+    }
+
+    private fetchHolidaysForCountry(country: string): ExportHoliday[] {
+        const hd = new Holidays(country, {types: this.types});
+
+        const result: ExportHoliday[] = [];
+
+        this.years.forEach(year => {
+            const holidays = hd.getHolidays(year) as ExportHoliday[];
+            if (holidays.length === 0) {
+                logger.warn('No holidays found for ' + country, year);
+                return [];
+            }
+
+            holidays.forEach(holiday => {
+                holiday.countryCode = country;
+                holiday.countryName = hd.getCountries()[country];
+            });
+
+            result.push(...holidays);
+        });
+
+        logger.info(`Fetched ${result.length} holidays for ${country}`);
+        return result;
+    }
+
+    private async ensureOutputDirectory() {
+        await fs.mkdir(this.outputPath, {recursive: true}).catch(err => {
+            throw new Error('Error creating output directory: ' + err);
+        });
+    }
+
+    private async writeHolidaysToFile(allHolidays: any[]) {
+        const fileName = this.fileName ? `${this.fileName}.json` : this.years.length === 1 ? `holidays-${this.years[0]}.json` : 'holidays.json';
+        const filePath = path.join(this.outputPath, fileName);
+        await fs.writeFile(filePath, JSON.stringify(allHolidays, null, 2)).catch(err => {
+            throw new Error('Error writing file: ' + err);
+        });
+        logger.info(`Successfully wrote ${allHolidays.length} holidays to ${filePath}`);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,17 @@
-import {default as Holidays, HolidaysTypes} from 'date-holidays';
 import logger from './logger.js';
-import fs from 'fs/promises';
-import path from 'node:path';
-
-const YEAR = 2024;
-const COUNTRIES: string[] = Object.keys(new Holidays().getCountries());
-const TYPES: HolidaysTypes.HolidayType[] = ['public'];
-const OUTPUT_PATH = 'output';
+import {HolidayExporter} from "./HolidayExporter.js";
 
 logger.info('Starting Holiday Converter...');
 
-type ExportHoliday = HolidaysTypes.Holiday & { country: string };
+const exporter = HolidayExporter.create()
+    .setFileName('holidays')
+    .setYears(2021, 2022)
+    .modifyJson((holiday) => {
+        return {
+            date: holiday.date.split(' ')[0],
+            country: holiday.countryCode
+        };
+    })
+    .build();
 
-async function ensureOutputDirectory() {
-    await fs.mkdir(OUTPUT_PATH, {recursive: true}).catch(err => {
-        throw new Error('Error creating output directory: ' + err);
-    });
-}
-
-function fetchHolidaysForCountry(country: string): ExportHoliday[] {
-    const hd = new Holidays(country, {types: TYPES});
-    const holidays = hd.getHolidays(YEAR) as ExportHoliday[];
-    if (holidays.length === 0) {
-        logger.warn('No holidays found for ' + country);
-        return [];
-    }
-
-    holidays.forEach(holiday => {
-        holiday.country = country;
-    });
-    logger.info(`Fetched ${holidays.length} holidays for ${country}`);
-    return holidays;
-}
-
-async function writeHolidaysToFile(allHolidays: ExportHoliday[]) {
-    const fileName = `holidays-${YEAR}.json`;
-    const filePath = path.join(OUTPUT_PATH, fileName);
-    await fs.writeFile(filePath, JSON.stringify(allHolidays, null, 2)).catch(err => {
-        throw new Error('Error writing file: ' + err);
-    });
-    logger.info(`Successfully wrote ${allHolidays.length} holidays to ${filePath}`);
-}
-
-async function main() {
-    await ensureOutputDirectory();
-    await writeHolidaysToFile(COUNTRIES.flatMap(fetchHolidaysForCountry));
-}
-
-main().catch(error => logger.error('Unexpected error:', error));
+await exporter.export();
